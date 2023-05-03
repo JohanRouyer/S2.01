38f32547abfd86c024957b02a96cdde328384a25
Eneko Etchegaray <114986169+Eneko6-4@users.noreply.github.com>
2023-05-03 15:51:40 +0200

    Add files via upload

---
+++ v1_version_lecteur-graphique/v1_lecteur_graphique/mainwindow.cpp
@@ -0,0 +1,17 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    statusBar()->showMessage("indicateur de mode");
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
